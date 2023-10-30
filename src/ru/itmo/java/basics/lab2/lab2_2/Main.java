d338e75fe232b3de6732d449a5bc7a1d0039eecc
ZhmakinAA <ZhmakinAA@hotmail.ru>
2023-10-30 14:32:54 +0300

    End struct grama

--- src/ru/itmo/java/basics/lab2/lab2_2/Main.java
+++ src/ru/itmo/java/basics/lab2/lab2_2/Main.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Document document = new Document("Contract");
         document.setBody("Body contract");
-        document.setFooter("CIO Жмакин А.А,");
+        document.setFooter("CIO Жмакин А.А.");
 
         document.Show();
 
