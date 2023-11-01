7b4b1b9e8f7d7d45df65e53dbfcc3cffe5908bda
ZhmakinAA <ZhmakinAA@hotmail.ru>
2023-11-01 10:08:40 +0300

    Final lab3_1

--- src/ru/itmo/java/basics/lab3/Lab3_1/JavaProgram.java
+++ src/ru/itmo/java/basics/lab3/Lab3_1/JavaProgram.java
@@ -2,7 +2,7 @@ package ru.itmo.java.basics.lab3.Lab3_1;
 
 public class JavaProgram {
     public static void main(String[] args) {
-        Study study = new Study("Изучение Java - это просто!");
+        Study study = new Study("Изучение Java -  это просто!");
         System.out.println(study.printCourse());
     }
 }
