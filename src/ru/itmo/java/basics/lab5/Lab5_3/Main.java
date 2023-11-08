bf5c008bfba44ae1fe0470a9a47c0e132942f637
ZhmakinAA <ZhmakinAA@hotmail.ru>
2023-11-08 18:00:32 +0300

    Final lab5 fix 5_3

--- src/ru/itmo/java/basics/lab5/Lab5_3/Main.java
+++ src/ru/itmo/java/basics/lab5/Lab5_3/Main.java
@@ -3,11 +3,13 @@ package ru.itmo.java.basics.lab5.Lab5_3;
 public class Main {
     public static void main(String[] args) {
         String text = "Есть много бяка Lorem Ipsum, но большинство из них бяка не всегда приемлемые модификации, например";
-        replaceByaka(text);
+        String source  = "бяка";
+        String target = "[вырезано цензурой]";
+        replaceByaka(text, source, target);
     }
 
-    public static void replaceByaka(String text) {
-        text = text.replace("бяка", "[вырезано цензурой]");
+    public static void replaceByaka(String text, String source, String target) {
+        text = text.replace(source, target);
         System.out.println(text);
     }
 }
